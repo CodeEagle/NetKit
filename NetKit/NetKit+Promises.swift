2763bfc098bc9d91ab1f397280506a997c3e22cf
CodeEagle <Stasura@hotmail.com>
2019-03-31 11:46:17 +0800

    chore: swift5 compatible

--- NetKit/NetKit+Promises.swift
+++ NetKit/NetKit+Promises.swift
@@ -1,17 +1,15 @@
-#if canImport(Promises)
-    import Promises
+import Promises
 
-    public extension NetKitRequestable {
-        func execute<T: Decodable>() -> Promise<T> {
-            return Promise<T>({ success, failure in
-                let hanlder: (Result<T>) -> Void = { result in
-                    switch result {
-                    case let .success(value): success(value)
-                    case let .failure(error): failure(error)
-                    }
+public extension NetKitRequestable {
+    func execute<T: Decodable>() -> Promise<T> {
+        return Promise<T>({ success, failure in
+            let hanlder: (Result<T>) -> Void = { result in
+                switch result {
+                case let .success(value): success(value)
+                case let .failure(error): failure(error)
                 }
-                self.execute(complete: hanlder)
-            })
-        }
+            }
+            self.execute(complete: hanlder)
+        })
     }
-#endif
+}
